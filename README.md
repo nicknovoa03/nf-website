5dfc492ae2aa72706a73537f5c3faa8db47633e5
Nick Novoa <nicknovoa03@gmail.com>
2022-10-19 11:14:29 -0500

    inital commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Natural Frameworks Website
+
+## Built With:
+- Next.js
+- React.js
+- Material UI
+
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Home } from 'views';
+const IndexPage = () => {
+  return <Home />;
+};
+
+export default IndexPage;
