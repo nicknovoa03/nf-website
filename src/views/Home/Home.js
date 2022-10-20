d9d1f650936fb27d57aeb7dbc6c99e7e17a950d9
Nick Novoa <nicknovoa03@gmail.com>
2022-10-19 22:47:52 -0500

    Web Chat Widget

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -17,52 +17,68 @@ const Home = () => {
   const theme = useTheme();
 
   return (
-    <Fluid>
-      <Container>
-        <Hero />
-      </Container>
-      <Container>
-        <Partners />
-      </Container>
-      <Container>
-        <a id="OnlineReviews" />
-        <OnlineReviews />
-      </Container>
-      <Container paddingY={'0 !important'}>
-        <Messaging />
-      </Container>
-      <Container>
-        <Payment />
-      </Container>
-      <Box
-        position={'relative'}
-        sx={{
-          backgroundColor: theme.palette.alternate.main,
-        }}
+    <>
+      <div
+        data-chat-widget
+        data-style="--chat-widget-primary-color: #188bf6; --chat-widget-active-color:#188bf6 ;--chat-widget-bubble-color: #188bf6"
+        data-location-id="d9EJPvHmPoXnwbaq1mCE"
+        data-prompt-avatar="https://widgets.leadconnectorhq.com/chat-widget/assets/defaultAvatar.png"
+        data-agency-name="Hexona Systems"
+        data-agency-website="www.hexonasystems.com"
+      ></div>
+      <script
+        src="https://widgets.leadconnectorhq.com/loader.js"
+        data-resources-url="https://widgets.leadconnectorhq.com/chat-widget/loader.js"
       >
+        {' '}
+      </script>
+      <Fluid>
         <Container>
-          <a id="LearnMore" />
-          <Contact />
+          <Hero />
+        </Container>
+        <Container>
+          <Partners />
+        </Container>
+        <Container>
+          <a id="OnlineReviews" />
+          <OnlineReviews />
+        </Container>
+        <Container paddingY={'0 !important'}>
+          <Messaging />
+        </Container>
+        <Container>
+          <Payment />
         </Container>
         <Box
-          component={'svg'}
-          preserveAspectRatio="none"
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          viewBox="0 0 1920 100.1"
+          position={'relative'}
           sx={{
-            width: '100%',
-            marginBottom: theme.spacing(-1),
+            backgroundColor: theme.palette.alternate.main,
           }}
         >
-          <path
-            fill={theme.palette.background.paper}
-            d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
-          ></path>
+          <Container>
+            <a id="LearnMore" />
+            <Contact />
+          </Container>
+          <Box
+            component={'svg'}
+            preserveAspectRatio="none"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            viewBox="0 0 1920 100.1"
+            sx={{
+              width: '100%',
+              marginBottom: theme.spacing(-1),
+            }}
+          >
+            <path
+              fill={theme.palette.background.paper}
+              d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
+            ></path>
+          </Box>
         </Box>
-      </Box>
-    </Fluid>
+      </Fluid>
+    </>
   );
 };
 
