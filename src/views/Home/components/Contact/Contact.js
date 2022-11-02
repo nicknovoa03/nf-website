4fc1aa0f36661292ceb453afd42cf7d72139739a
Nick Novoa <nicknovoa03@gmail.com>
2022-11-02 18:18:35 -0500

    Book Demo

--- src/views/Home/components/Contact/Contact.js
+++ src/views/Home/components/Contact/Contact.js
@@ -5,6 +5,8 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import { Map } from './components';
 import Script from 'next/script';
+import { Divider, Typography } from '@mui/material';
+import Card from '@mui/material';
 
 const Contact = () => {
   const theme = useTheme();
@@ -18,15 +20,43 @@ const Contact = () => {
         <Grid item xs={12} md={6}>
           <Map />
         </Grid>
-        <Grid item container xs={12} md={6} alignItems={'center'}>
-          <iframe
-            src="https://api.leadconnectorhq.com/widget/form/wihukON3WCRDzUaj5U6F"
-            style={{ border: 'none', width: '100%' }}
-          ></iframe>
-          <Script
-            id="Contact Form"
-            src="https://api.leadconnectorhq.com/js/form_embed.js"
-          ></Script>
+        <Grid item xs={12} md={6}>
+          <Box
+            sx={{
+              backgroundColor: '#ffffff',
+              borderRadius: '16px',
+            }}
+            paddingY={3}
+          >
+            <Box marginBottom={2} alignItems={'center'}>
+              <Typography
+                marginBottom={1}
+                align="center"
+                variant="h4"
+                color={theme.palette.primary.main}
+                sx={{ fontWeight: 700 }}
+              >
+                Book A Demo
+              </Typography>
+              <Divider
+                color={'black'}
+                variant={'inset'}
+                sx={{ color: 'white' }}
+              />
+            </Box>
+            <Box alignContent={'center'}>
+              <iframe
+                src="https://api.leadconnectorhq.com/widget/booking/2oKTgJquhvSYH3eQR0Ea"
+                style={{ border: 'none', width: '100%', overflow: 'hidden' }}
+                scrolling="no"
+                id="msgsndr-calendar"
+              ></iframe>
+              <Script
+                src="https://api.leadconnectorhq.com/js/embed.js"
+                type="text/javascript"
+              ></Script>
+            </Box>
+          </Box>
         </Grid>
       </Grid>
     </Box>
