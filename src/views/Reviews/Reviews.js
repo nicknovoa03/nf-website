71e4b4354e746b2ec2eb5bf4e6269bd98510d975
Nick Novoa <nicknovoa03@gmail.com>
2022-11-03 17:09:22 -0500

    Reviews without content

--- src/views/Reviews/Reviews.js
+++ src/views/Reviews/Reviews.js
@@ -1,7 +1,31 @@
+import Container from 'components/Container';
 import { Main } from 'layouts';
 import React from 'react';
+import CTA from './components/CTA';
+import CTA2 from './components/CTA2';
+import Hero from './components/Hero';
+import Info from './components/Info';
+import LogoGrid from './components/LogoGrid';
 
 const Reviews = () => {
-  return <Main></Main>;
+  return (
+    <Main>
+      <Container>
+        <Hero />
+      </Container>
+      <Container>
+        <Info />
+      </Container>
+      <Container>
+        <CTA />
+      </Container>
+      <Container>
+        <LogoGrid />
+      </Container>
+      <Container>
+        <CTA2 />
+      </Container>
+    </Main>
+  );
 };
 export default Reviews;
