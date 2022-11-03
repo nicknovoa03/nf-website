6544cc407cee8e27fa2db0952df16e3c9d903acd
Nick Novoa <nicknovoa03@gmail.com>
2022-11-03 15:48:52 -0500

    initial Reviews

---
+++ src/pages/reviews.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Reviews } from 'views';
+const IndexPage = () => {
+  return <Reviews />;
+};
+
+export default IndexPage;
---
+++ src/views/Reviews/Reviews.js
@@ -0,0 +1,7 @@
+import { Main } from 'layouts';
+import React from 'react';
+
+const Reviews = () => {
+  return (<Main></Main>);
+};
+export default Reviews;
