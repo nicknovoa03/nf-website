5fd23c9f03660197fa21307c948829a862dc9361
Nick Novoa <nicknovoa03@gmail.com>
2022-12-06 17:41:31 -0600

    eslint fix

--- src/views/DigitalMarketing/components/Features/FeatureCardsWithColorfullIconsAndLearnMoreLink.js
+++ src/views/DigitalMarketing/components/Features/FeatureCardsWithColorfullIconsAndLearnMoreLink.js
@@ -8,20 +8,23 @@ import Avatar from '@mui/material/Avatar';
 import { alpha } from '@mui/material/styles';
 import { colors, Divider, useMediaQuery } from '@mui/material';
 
-import AllInboxIcon from '@mui/icons-material/AllInbox';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
-import MarkUnreadChatAltOutlinedIcon from '@mui/icons-material/MarkUnreadChatAltOutlined';
-import PhoneMissedIcon from '@mui/icons-material/PhoneMissed';
-import PaidOutlinedIcon from '@mui/icons-material/PaidOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import CodeIcon from '@mui/icons-material/Code';
+import ManageAccountsOutlinedIcon from '@mui/icons-material/ManageAccountsOutlined';
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
+import AutoGraphOutlinedIcon from '@mui/icons-material/AutoGraphOutlined';
+import GroupsIcon from '@mui/icons-material/Groups';
+import AnalyticsOutlinedIcon from '@mui/icons-material/AnalyticsOutlined';
+import AddTaskOutlinedIcon from '@mui/icons-material/AddTaskOutlined';
 import { useTheme } from '@emotion/react';
 import Container from 'components/Container';
 
 const mock = [
   {
-    color: colors.yellow[700],
+    color: colors.amber[700],
     title: 'Website Development',
     subtitle:
-      'Build and upgrade websites with automations, Personalized messages for clients, Reviews, Discounts, Etc.',
+      'Build and upgrade websites with Automations, Personalized messages for clients, Reviews, Discounts, Etc.',
     icon: (
       <svg
         height={30}
@@ -30,12 +33,12 @@ const mock = [
         fill="none"
         viewBox="0 0 24 24"
       >
-        <StarBorderIcon />
+        <CodeIcon />
       </svg>
     ),
   },
   {
-    color: colors.deepOrange[500],
+    color: colors.cyan[500],
     title: 'Email Marketing',
     subtitle:
       'Never miss or ghost any potential leads again. Assist in email marketing, including custom email templates and overall strategy.',
@@ -47,12 +50,12 @@ const mock = [
         fill="none"
         viewBox="0 0 24 24"
       >
-        <AllInboxIcon />
+        <EmailOutlinedIcon />
       </svg>
     ),
   },
   {
-    color: colors.cyan[500],
+    color: colors.deepOrange[300],
     title: 'Social Media Management',
     subtitle:
       'Our Social Media Management team is ready 24/7 to post, engage, answer questions, etc. on your accounts',
@@ -64,7 +67,7 @@ const mock = [
         fill="none"
         viewBox="0 0 24 24"
       >
-        <MarkUnreadChatAltOutlinedIcon />
+        <ManageAccountsOutlinedIcon />
       </svg>
     ),
   },
@@ -81,12 +84,12 @@ const mock = [
         fill="none"
         viewBox="0 0 24 24"
       >
-        <PaidOutlinedIcon />
+        <AutoGraphOutlinedIcon />
       </svg>
     ),
   },
   {
-    color: colors.red[500],
+    color: colors.amber[600],
     title: 'Reviews and Ratings',
     subtitle:
       'Keep track of what your customers are saying about you. Respond to reviews straight from the application.',
@@ -96,9 +99,8 @@ const mock = [
         width={30}
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
-        viewBox="0 0 24 24"
       >
-        <PhoneMissedIcon />
+        <StarBorderOutlinedIcon />
       </svg>
     ),
   },
@@ -109,24 +111,17 @@ const mock = [
       'Work with our team and train to understand the importance of keeping an online presence',
     icon: (
       <svg
-        height={24}
-        width={24}
+        height={30}
+        width={30}
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
       >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
-        />
+        <GroupsIcon />
       </svg>
     ),
   },
   {
-    color: colors.red[500],
+    color: colors.deepOrange[500],
     title: 'Audience Analytics',
     subtitle:
       'Track all performance on posts, interactions on ad spend, and conversion all from the palm of your hand',
@@ -136,32 +131,24 @@ const mock = [
         width={30}
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
-        viewBox="0 0 24 24"
       >
-        <PhoneMissedIcon />
+        <AnalyticsOutlinedIcon />
       </svg>
     ),
   },
   {
-    color: colors.lightBlue[500],
+    color: colors.green[500],
     title: 'CRM',
     subtitle:
       'Keep track of all your leads and customers. Grow your audience & know where new leads are coming from',
     icon: (
       <svg
-        height={24}
-        width={24}
+        height={30}
+        width={30}
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
       >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
-        />
+        <AddTaskOutlinedIcon />
       </svg>
     ),
   },
